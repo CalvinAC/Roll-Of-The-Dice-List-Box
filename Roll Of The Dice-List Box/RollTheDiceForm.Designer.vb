c254431b2a30b50996ce99da07b8257b8fcbfff9
cameron coxen <coxecame@isu.edu>
2020-10-14 20:48:34 -0600

    Finished it

--- Roll Of The Dice-List Box/RollTheDiceForm.Designer.vb
+++ Roll Of The Dice-List Box/RollTheDiceForm.Designer.vb
@@ -40,7 +40,8 @@ Partial Class RollTheDiceForm
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem1})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(814, 28)
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(610, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -48,50 +49,53 @@ Partial Class RollTheDiceForm
         '
         Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpToolStripMenuItem, Me.ClearToolStripMenuItem})
         Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
-        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(60, 24)
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
         Me.MenuToolStripMenuItem.Text = "Menu"
         '
         'HelpToolStripMenuItem
         '
         Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
-        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.HelpToolStripMenuItem.Text = "Roll"
         '
         'ClearToolStripMenuItem
         '
         Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
-        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.ClearToolStripMenuItem.Text = "Clear"
         '
         'HelpToolStripMenuItem1
         '
         Me.HelpToolStripMenuItem1.Name = "HelpToolStripMenuItem1"
-        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(55, 24)
+        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(44, 20)
         Me.HelpToolStripMenuItem1.Text = "Help"
         '
         'RollButton
         '
-        Me.RollButton.Location = New System.Drawing.Point(287, 405)
+        Me.RollButton.Location = New System.Drawing.Point(215, 329)
+        Me.RollButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RollButton.Name = "RollButton"
-        Me.RollButton.Size = New System.Drawing.Size(133, 72)
+        Me.RollButton.Size = New System.Drawing.Size(100, 58)
         Me.RollButton.TabIndex = 1
         Me.RollButton.Text = "&Roll"
         Me.RollButton.UseVisualStyleBackColor = True
         '
         'ClearButton
         '
-        Me.ClearButton.Location = New System.Drawing.Point(455, 405)
+        Me.ClearButton.Location = New System.Drawing.Point(341, 329)
+        Me.ClearButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.ClearButton.Name = "ClearButton"
-        Me.ClearButton.Size = New System.Drawing.Size(133, 72)
+        Me.ClearButton.Size = New System.Drawing.Size(100, 58)
         Me.ClearButton.TabIndex = 2
         Me.ClearButton.Text = "&Clear"
         Me.ClearButton.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(624, 405)
+        Me.ExitButton.Location = New System.Drawing.Point(468, 329)
+        Me.ExitButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(133, 72)
+        Me.ExitButton.Size = New System.Drawing.Size(100, 58)
         Me.ExitButton.TabIndex = 3
         Me.ExitButton.Text = "&Exit"
         Me.ExitButton.UseVisualStyleBackColor = True
@@ -99,23 +103,24 @@ Partial Class RollTheDiceForm
         'RollDiceListBox
         '
         Me.RollDiceListBox.FormattingEnabled = True
-        Me.RollDiceListBox.ItemHeight = 16
-        Me.RollDiceListBox.Location = New System.Drawing.Point(12, 31)
+        Me.RollDiceListBox.Location = New System.Drawing.Point(9, 25)
+        Me.RollDiceListBox.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RollDiceListBox.Name = "RollDiceListBox"
-        Me.RollDiceListBox.Size = New System.Drawing.Size(765, 340)
+        Me.RollDiceListBox.Size = New System.Drawing.Size(575, 277)
         Me.RollDiceListBox.TabIndex = 4
         '
         'RollTheDiceForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(814, 519)
+        Me.ClientSize = New System.Drawing.Size(610, 422)
         Me.Controls.Add(Me.RollDiceListBox)
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.RollButton)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "RollTheDiceForm"
         Me.Text = "Roll The Dice"
         Me.MenuStrip1.ResumeLayout(False)
--- Roll Of The Dice-List Box/RollTheDiceForm.vb
+++ Roll Of The Dice-List Box/RollTheDiceForm.vb
@@ -13,32 +13,31 @@ Public Class RollTheDiceForm
     Sub RollDie()
 
         Dim dieRoll As Integer
-        Dim rolls(12) As Integer
-        Dim row As Integer
+        Dim rolls(10) As Integer
         Dim text1 As String
         Dim text2 As String
         Dim dashLines As String
-        Dim listBoxTitle As String
+        Randomize()
 
         'Rolls the dice 1000 times
         For i = 1 To 1000
-            dieRoll = GetRandomNumber(1, 11)
-            rolls(dieRoll) += 1
+            dieRoll = CInt(GetRandomNumber(1, 6))
+            rolls(dieRoll - 2) += 1
         Next
 
         'Creates place holder for data from the die rolls
         For i = 2 To 12
-            text1 = (text1 & String.Format("{0,11}", i & "|"))
+            text1 = text1 & String.Format("{0,11}", i & "|")
         Next
 
         For i = 0 To 10
-            text2 = (text2 & String.Format("{0,10}", rolls(i) & "|"))
+            text2 = text2 & String.Format("{0,10}", rolls(i) & "|")
         Next
 
         dashLines = (StrDup(144, "-"))
 
         'Prints the text into the list box
-        RollDiceListBox.Items.Add("Roll The Dice")
+        RollDiceListBox.Items.Add(vbTab & vbTab & vbTab & vbTab & "Roll The Dice")
         RollDiceListBox.Items.Add(dashLines)
         RollDiceListBox.Items.Add(text1)
         RollDiceListBox.Items.Add(dashLines)
@@ -50,13 +49,19 @@ Public Class RollTheDiceForm
 
     End Sub
 
-    'Randomizes the numbers between 1 to 12
-    Function GetRandomNumber(ByVal min As Integer, ByVal max As Integer) As Integer
-        Dim number As Single
-        Randomize()
-        number = CInt(Int(12 * Rnd()) + 1)
-        number = ((max - min + 1) * Rnd()) + min
-        Return CInt((number))
+    Function GetRandomNumber(ByVal min As Single, ByVal max As Single) As Single
+        Dim roll1, roll2 As Double
+        Dim sum As Integer
+        'rolls 2 dice and add their values together
+        Do
+            roll1 = (max * Rnd()) + 0.5
+            roll2 = (max * Rnd()) + 0.5
+        Loop While roll1 < min - 0.5 Or roll1 >= max + 0.5 Or
+            roll2 < min - 0.5 Or roll2 >= max + 0.5
+
+
+        sum = CInt(roll1) + CInt(roll2)
+        Return sum
 
     End Function
     Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
@@ -68,8 +73,7 @@ Public Class RollTheDiceForm
     End Sub
 
     Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
-        RollDiceListBox.Items.Add("Clear the dice rolls by selecting the Clear button, or by pressing the C key")
-
+        RollDiceListBox.Items.Clear()
     End Sub
     Private Sub RollTheDiceForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Text = "Yeet Le Dice"
@@ -79,6 +83,15 @@ Public Class RollTheDiceForm
     End Sub
 
     Private Sub HelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem.Click
+        RollDie()
+    End Sub
+
+    Private Sub MenuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MenuToolStripMenuItem.Click
+        RollDiceListBox.Items.Clear()
+    End Sub
+
+    Private Sub HelpToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem1.Click
+        RollDiceListBox.Items.Add("Clear the dice rolls by selecting the Clear button, or by pressing the C key")
         RollDiceListBox.Items.Add("Roll the dice by selecting the Roll button, or by pressing the R key")
     End Sub
 End Class
